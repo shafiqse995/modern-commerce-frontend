fb2bcccc2397c0598b9a5767cab6c452061f46da
Dhurv-Gopani <dhruvkumar.gopani@stud.fra-uas.de>
2025-01-27 22:59:04 +0500

    feat: initial setup

---
+++ src/main.tsx
@@ -0,0 +1,39 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
+import type { AxiosError } from 'axios';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+import { routeTree } from './routeTree.gen';
+
+const router = createRouter({ routeTree });
+const queryClient = new QueryClient();
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+declare module '@tanstack/react-query' {
+  interface Register {
+    defaultError: AxiosError;
+  }
+}
+
+document.title = import.meta.env.VITE_APP_TITLE;
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </React.StrictMode>,
+  );
+}
